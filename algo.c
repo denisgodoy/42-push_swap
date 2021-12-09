6c6c8a04d9322f7df57becc78e791c5bc3310994
Denis Godoy <denisgodoy1@gmail.com>
2021-12-08 22:59:36 -0300

    update push_swap

--- algo.c
+++ algo.c
@@ -6,7 +6,7 @@
 /*   By: degabrie <degabrie@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/08 18:24:57 by degabrie          #+#    #+#             */
-/*   Updated: 2021/12/08 21:35:05 by degabrie         ###   ########.fr       */
+/*   Updated: 2021/12/08 22:47:23 by degabrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,52 +14,20 @@
 
 static int	ft_operation(char *operation);
 static int	ft_small_algo(t_ps *push_swap, int *args);
-static int	ft_2stack_algo(t_ps *push_swap, int *args);
 
 int	ft_algorithm(t_ps *push_swap)
 {
-	if (ft_is_sorted(push_swap))
-		return (0);
-	else if (push_swap->len <= 3)
+	int	midpoint;
+
+	midpoint = ft_is_sorted(push_swap);
+	if (push_swap->len <= 3)
 		return (ft_small_algo(push_swap, push_swap->args));
-	else if (push_swap->len <= 5)
-		return (ft_2stack_algo(push_swap, push_swap->args));
 	return (1);
 }
 
 static int	ft_operation(char *operation)
 {
 	ft_putendl_fd(operation, STDIN);
-	return (1);
-}
-
-static int	ft_2stack_algo(t_ps *push_swap, int *args)
-{
-	int	*stack_a;
-	int	*stack_b;
-	int	i;
-	int	j;
-
-	push_swap->len -= 3;
-	stack_b = (int *)malloc(push_swap->len * sizeof(int));
-	i = -1;
-	while (++i < push_swap->len)
-	{
-		stack_b[i] = args[i];
-		ft_operation("pb");
-	}
-	ft_small_algo(push_swap, stack_b);
-	push_swap->len = 3;
-	stack_a = (int *)malloc(push_swap->len * sizeof(int));
-	j = -1;
-	while (++j < push_swap->len)
-		stack_a[j] = args[i++];
-	ft_small_algo(push_swap, stack_a);
-	while (i - 3)
-	{
-		ft_operation("pa");
-		i--;
-	}
 	return (1);
 }
 
@@ -84,6 +52,11 @@ static int	ft_small_algo(t_ps *push_swap, int *args)
 	{
 		ft_operation("ra");
 		ft_operation("sa");
+	}
+	else if (args[i] < args[push_swap->len - 1])
+	{
+		ft_operation("sa");
+		return (ft_operation("ra"));
 	}
 	return (ft_operation("rra"));
 }
--- ft_is_sorted.c
+++ ft_is_sorted.c
@@ -6,25 +6,69 @@
 /*   By: degabrie <degabrie@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/25 00:01:20 by degabrie          #+#    #+#             */
-/*   Updated: 2021/12/08 21:36:24 by degabrie         ###   ########.fr       */
+/*   Updated: 2021/12/08 22:54:55 by degabrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	"push_swap.h"
 
+static int	*ft_intdup(int *arr, int len);
+static int	ft_get_midpoint(int flag, int *arr, t_ps *push_swap);
+
 int	ft_is_sorted(t_ps *push_swap)
 {
+	int	temp;
+	int	*arr;
+	int	flag;
 	int	i;
 	int	j;
 
+	arr = ft_intdup(push_swap->args, push_swap->len);
+	flag = 0;
 	i = -1;
 	while (++i < push_swap->len)
 	{
 		j = i;
 		while (++j < push_swap->len)
-			if (push_swap->args[i] > push_swap->args[j])
-				return (0);
+		{
+			if (arr[i] > arr[j])
+			{
+				flag = 1;
+				temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+		}
 	}
-	free(push_swap->args);
-	return (1);
+	return (ft_get_midpoint(flag, arr, push_swap));
+}
+
+static int	*ft_intdup(int *arr, int len)
+{
+	int	*new_arr;
+	int	i;
+
+	new_arr = (int *)malloc(len * sizeof(int));
+	i = -1;
+	while (++i < len)
+		new_arr[i] = arr[i];
+	return (new_arr);
+}
+
+static int	ft_get_midpoint(int flag, int *arr, t_ps *push_swap)
+{
+	int	median;
+
+	if (!flag)
+	{
+		free(arr);
+		free(push_swap->args);
+		exit(EXIT_FAILURE);
+	}
+	else if (push_swap->len % 2 != 0)
+		median = arr[push_swap->len / 2];
+	else
+		median = (arr[push_swap->len / 2] + arr[push_swap->len / 2 - 1]) / 2;
+	free(arr);
+	return (median);
 }
