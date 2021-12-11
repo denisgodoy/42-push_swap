3159750cd6a97555fd6356fd3d022b38e9b456a8
Denis Godoy <denisgodoy1@gmail.com>
2021-12-10 21:19:49 -0300

    update push_swap

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: degabrie <degabrie@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/19 21:19:18 by degabrie          #+#    #+#             */
-/*   Updated: 2021/12/10 21:03:29 by degabrie         ###   ########.fr       */
+/*   Updated: 2021/12/10 21:18:49 by degabrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,21 +25,24 @@ int	main(int argc, char **argv)
 	}
 	ft_init_stacks(&push_swap);
 	ft_algorithm(&push_swap);
+	printf("\nstack b size %d\n", ft_llsize(push_swap.stack_b));
+	ft_print_stack(&push_swap.stack_b);
+	printf("\nstack a size %d\n", ft_llsize(push_swap.stack_a));
+	ft_print_stack(&push_swap.stack_a);
 	ft_llclear(&push_swap.stack_a, &free);
-	// printf("\nstack b size %d\n", ft_llsize(push_swap.stack_b));
-	// ft_print_stack(&push_swap.stack_b);
-	// printf("\nstack a size %d\n", ft_llsize(push_swap.stack_a));
-	// ft_print_stack(&push_swap.stack_a);
 	exit(EXIT_SUCCESS);
 }
 
 static void	ft_print_stack(t_sort **stack)
 {
-	while (*stack != NULL)
+	t_sort	*temp;
+
+	temp = *stack;
+	while (temp != NULL)
 	{
-		printf("\naddr %p\n", *stack);
-		printf("next %p\n", (*stack)->next);
-		printf("num %d\n", (*stack)->num);
-		*stack = (*stack)->next;
+		printf("\naddr %p\n", temp);
+		printf("next %p\n", temp->next);
+		printf("num %d\n", temp->num);
+		temp = temp->next;
 	}
 }
